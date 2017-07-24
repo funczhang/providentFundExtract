08161f9659b17df6195dea447879216ef39b20d1
NickZhang <funczhang@users.noreply.github.com>
2017-07-24 10:09:36 +0800

    新增材料列表展示组件
    新增材料列表展示组件，修改公共样式

---
+++ src/components/common/mList.vue
@@ -0,0 +1,49 @@
+<template lang="pug">
+  div(class="m-list")
+    p(@click="toggle") {{title}}
+    ul(ref="list")
+        li(v-for="item in mList") {{item}}
+</template>
+<script>
+export default {
+    props: ['title', 'mList'],
+    data() {
+        return {}
+    },
+    methods: {
+        toggle() {
+            let status = this.$refs.list.style.display
+            if (status == '' || status == 'block') {
+                status = 'none';
+            } else {
+                status = 'block';
+            }
+            console.log(status);
+            debugger
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+.m-list {
+    background-color: #fff;
+    p {
+        margin-left: 15px;
+        height: 2.4rem;
+        border-top: 1px solid #e5e5e5;
+        border-bottom: 1px solid #e5e5e5;
+        font-size: 0.75rem;
+        line-height: 2.4rem;
+        color: #000;
+    }
+    ul {
+        margin-left: 15px;
+    }
+    li {
+        padding: 0.5rem 0;
+        height: 1rem;
+        line-height: 1rem;
+        border-bottom: 1px solid #e5e5e5;
+    }
+}
+</style>
--- src/pages/common/applyForm.vue
+++ src/pages/common/applyForm.vue
@@ -2,31 +2,39 @@
   div(class="form")
     div(class="module-head") 
         h3 申请人信息
-    <mt-field label="姓名" placeholder="请填写姓名" :attr="{ maxlength: 50 }"></mt-field>
-    <mt-field label="身份证号码" placeholder="请填写身份证号码" type="text" :attr="{ maxlength: 18 }"></mt-field>
-    <mt-field label="手机号码" placeholder="请填写手机号码" type="tel" :attr="{ maxlength: 11 }"></mt-field>
-    <mt-field label="单位名称(全称)" placeholder="请填写单位名称" type="text" :attr="{ maxlength: 50 }"></mt-field>
-    <mt-field label="收款人" placeholder="请填写收款人" type="text" :attr="{ maxlength: 50 }"></mt-field>
+    mt-field(label="姓名" placeholder="请填写姓名" :attr="{ maxlength: 50 }")
+    mt-field(label="身份证号码" placeholder="请填写身份证号码" type="text" :attr="{ maxlength: 18 }")
+    mt-field(label="手机号码" placeholder="请填写手机号码" type="tel" :attr="{ maxlength: 11 }")
+    mt-field(label="单位名称(全称)" placeholder="请填写单位名称" type="text" :attr="{ maxlength: 50 }")
+    mt-field(label="收款人" placeholder="请填写收款人" type="text" :attr="{ maxlength: 50 }")
     div(class="acount-field")
         div(class="inner-box")
             <mt-field label="提取金额(元)" placeholder="请填写需要提取的金额" type="number" :attr="{ maxlength: 20 }"></mt-field>
-            p 当前余额： 44556677.000元
-                
-    <mt-field label="开户银行" placeholder="请填写申请人本人储蓄卡开户银行" type="text" :attr="{ maxlength: 18 }"></mt-field>
-    <mt-field label="储蓄卡账号" placeholder="请填写申请人本人储蓄卡开户银行" type="number" :attr="{ maxlength: 18 }"></mt-field>
-    <v-button title="申请" @btnClick="applyFund()"></v-button>
+            p 当前余额： 44556677.000元               
+    mt-field(label="开户银行" placeholder="请填写申请人本人储蓄卡开户银行" type="text" :attr="{ maxlength: 18 }")
+    mt-field(label="储蓄卡账号" placeholder="请填写申请人本人储蓄卡开户银行" type="number" :attr="{ maxlength: 18 }")
+    div(class="module-head") 
+        h3 申请材料 
+            span (支持的类型：jpg,png;不超过200KB)
+    //- <mt-cell title="单位出具的《住房公积金提取申请书》1份" value=""></mt-cell>
+    //- <mt-cell title="单位出具的《住房公积金提取申请书》1份单位出具的《住房公积金提取申请书》1份" value=""></mt-cell>    
+    v-list(v-for="item in list",:title="item.title",:mList="item.mList",:key="item.id")
+    v-button(title="申请" @btnClick="applyFund()")
 </template>
 <script>
 //该页面使用mint-ui的field组件 button使用自定义组件
-import { Field, Toast } from 'mint-ui';
+import { Field, Toast,Cell } from 'mint-ui';
 import button from '../../components/common/button'
+import mList from '../../components/common/mList'
 export default {
     components: {
-        'v-button': button
+        'v-button': button,
+        'v-list':mList
     },
     data() {
         return {
-
+            list:[{title:'一寸身份证照片',mList:['材料11111','材料22222','材料33333','材料4444'],id:'1'},
+            {title:'户口本照片',mList:['材料11111','材料22222','材料33333','材料4444'],id:'1'}]
         }
     },
     methods: {
@@ -59,17 +67,25 @@ export default {
 @f05: #ff0055;
 @c0: #000;
 @cf: #fff;
+//通用表单头部
 .module-head {
-    padding: 0.5rem 0;
+    margin-top:0.5rem;
+    padding: 0.6rem 0;
     background: #fff;
     h3 {
-        border-left: 3px solid @f05;
+        border-left: 2px solid @f05;
         font-size: @font-mid;
         text-indent: 0.5rem;
-        color: #000;
+        color: #999;
+    }
+    span{
+        font-size:0.6rem;
     }
 }
-
+.mint-cell-title{
+    
+}
+//修改账户余额表单项
 .acount-field {
     background: @cf;
     .inner-box {
